d74c1f922616251ce4a28afb3c7ecd4f514307ed
BobLakensi <montgomery.okdaily@gmail.com>
2017-05-16 19:04:50 -0700

    move cube player with arrow keys

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	
+	public float speed;
+
+	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void FixedUpdate () {
+		float moveHorizonal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizonal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
 	}
 }
