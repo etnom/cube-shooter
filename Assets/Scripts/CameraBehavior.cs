1b806378aa7261f3cf0557a4c1788eb328745856
BobLakensi <montgomery.okdaily@gmail.com>
2017-05-17 15:27:35 -0700

    camera follow player

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraBehaviro : MonoBehaviour {
+public class CameraBehavior : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-		
+		offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
 	}
 }
