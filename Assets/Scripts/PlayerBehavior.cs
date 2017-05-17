be153660c93c3ba67f8df41e32d7d5401a3603c6
BobLakensi <montgomery.okdaily@gmail.com>
2017-05-17 15:46:35 -0700

    fix firerate by adding fireRate, not nextShot

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -21,7 +21,7 @@ public class PlayerBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if ( Input.GetButton("Fire1") && Time.time > nextFire) {
-			nextFire = Time.time + nextFire;
+			nextFire = Time.time + fireRate;
 			Instantiate(shot, shotSpawn.transform.position, shotSpawn.transform.rotation);
 		}
 	}
