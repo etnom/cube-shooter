c2a222ff8e55226e66bcd70bc244e3d53c4cdbb6
BobLakensi <montgomery.okdaily@gmail.com>
2017-05-17 15:54:20 -0700

    move bullet when instantiated

---
+++ Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour {
+
+	public float speed;
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
